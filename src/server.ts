e16b241ef43e728ec69bc77d2c06768040c560ea
Vinney <thivinney@gmail.com>
2022-04-02 01:18:26 -0300

    Add kafkajs in project

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+// import { Kafka } from 'kafkajs'
+
 import routers from './routers'
+
+// const kafka = new Kafka({
+//   clientId: 'my-app',
+//   brokers: ['kafka1:9092', 'kafka2:9092']
+// })
+
 const app = express()
 
+// app.use((req, res, next) => {
+//   req.producer = kafka.producer()
+// })
 app.use(bodyParser.json())
 app.use((req, res, next) => {
   res.set('Access-Control-Allow-Origin', '*')
