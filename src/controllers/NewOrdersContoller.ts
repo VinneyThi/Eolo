aa43c13eb6bdf19d64e2da694ab4fa36b5d273b0
Vinney <thivinney@gmail.com>
2022-04-02 15:16:15 -0300

    Add logic producer connect to kafka

--- src/controllers/NewOrdersContoller.ts
+++ src/controllers/NewOrdersContoller.ts
@@ -1,12 +1,21 @@
 import newOrderModel from '@models/newOrder'
 import { Request, Response } from 'express'
-
+import { CompressionTypes } from 'kafkajs'
 export class NewOrderController {
   static async CretedNewOrder (req :Request, res :Response) {
     const reqBody = req.body
     console.log(reqBody)
     try {
       await newOrderModel.create(reqBody)
+      await req.producer.connect()
+      await req.producer.send({
+        topic: 'newOrder_Processing',
+        compression: CompressionTypes.GZIP,
+        messages: [
+          { value: JSON.stringify(reqBody) }
+        ]
+      })
+      await req.producer.disconnect()
       res.status(201)
       res.end()
     } catch (error) {
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,28 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-// import { Kafka } from 'kafkajs'
+import { Kafka, logLevel } from 'kafkajs'
 
 import routers from './routers'
 
-// const kafka = new Kafka({
-//   clientId: 'my-app',
-//   brokers: ['kafka1:9092', 'kafka2:9092']
-// })
+/**
+ * Faz conexão com o Kafka
+ */
+const kafka = new Kafka({
+  clientId: 'api',
+  brokers: ['localhost:9092'],
+  logLevel: logLevel.WARN,
+  retry: {
+    initialRetryTime: 300,
+    retries: 10
+  }
+})
 
 const app = express()
 
-// app.use((req, res, next) => {
-//   req.producer = kafka.producer()
-// })
+app.use((req, res, next) => {
+  req.producer = kafka.producer()
+  next()
+})
 app.use(bodyParser.json())
 app.use((req, res, next) => {
   res.set('Access-Control-Allow-Origin', '*')
